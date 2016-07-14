c622b72f9c444eecf0da19a49d583192af8922ff
Ryan Kane <Kane@Ryans-MacBook-Pro.local>
2016-07-14 11:57:22 -0600

    display specific tea info

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,17 @@
-app.controller('HomeController', function($scope){
+app.controller('HomeController', function($scope, $http){
+
+  $scope.state = {
+    teas: 'loading'
+  }
+
+  $http.get('teas.json')
+  .then(function (teas) {
+    console.log(teas.data);
+    $scope.state.teas = teas.data;
+  })
+  .catch(function(e) {
+    console.log(e);
+  })
 
 });
 
