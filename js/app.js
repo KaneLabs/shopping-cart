1f12d4ec70e336d99d3be62c303d02bc79abeeed
Ryan Kane <Kane@Ryans-MacBook-Pro.local>
2016-07-14 11:24:26 -0600

    Navigation stories complete :)

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function ($routeProvider, $locationProvider) {
+
+  $routeProvider
+  .when('/', {
+    templateUrl: '../partials/home.html',
+    controller: 'HomeController'
+  })
+  .when('/about', {
+    templateUrl: '../partials/about.html',
+    controller: 'AboutController'
+  })
+  .when('/contact', {
+    templateUrl: '../partials/contact.html',
+    controller: 'ContactController'
+  })
+  .otherwise({
+    redirectTo: '/'
+  })
+
+  $locationProvider.html5Mode(true);
+});
---
+++ js/controllers.js
@@ -0,0 +1,11 @@
+app.controller('HomeController', function($scope){
+
+});
+
+app.controller('AboutController', function($scope){
+
+});
+
+app.controller('ContactController', function($scope){
+
+});
